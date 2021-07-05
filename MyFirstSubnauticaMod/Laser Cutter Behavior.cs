8bf1a30b49c167812cb11b6e1fba49e3c1e28456
bakemonawa <86772183+bakemonawa@users.noreply.github.com>
2021-07-05 12:01:46 +0800

    Add files via upload

---
+++ MyFirstSubnauticaMod/Laser Cutter Behavior.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace MyFirstSubnauticaMod
+{
+    [RequireComponent(typeof(EnergyMixin))]
+    [HarmonyPatch(typeof(LaserCutter))]
+    [HarmonyPatch("Start")]
+
+    
+
+    public class LaserCutterMK2 : LaserCutter
+    {
+        void Update()
+
+        {
+            base.Update();
+            // do ur own object tracking here
+
+
+            if (GameInput.GetButtonHeld(GameInput.Button.RightHand);
+
+            {
+                energyMixin.ConsumeEnergy(1f);
+                
+            }
+
+            {
+
+                var entityRoot = Utils.GetEntityRoot(go) ?? go;
+
+                float LaserDamage = 40;
+
+                entityRoot?.GetComponentInChildren<LiveMixin>()?.TakeDamage(LaserDamage, DamageType.Heat);
+                Logger.Log(Logger.Level.Debug, $"Laser damage was: null," +
+                $" is now: {LaserDamage}");
+
+            }
+
+                          
+
+        }
+
+
+
+
+
+
+    }
+
+}
+
+
+
+    
+    
---
+++ MyFirstSubnauticaMod/LaserCutterMk2Prefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+
+
+namespace MyFirstSubnauticaMod
+{
+    public class LaserCutterMk2Prefab
+    {
+        public LaserCutterMk2Prefab(string classId, string prefabFileName, TechType techType = TechType.LaserCutter) : base(classId, prefabFileName, techType)
+        {
+        }
+
+        GameObject LaserCutterMk2Prefab.GetGameObject()
+        {
+            GameObject LaserCutterMk2 = Resources.Load<GameObject>("WorldEntities/Tools/LaserCutter");
+            
+            LaserCutterMk2.AddComponent<EnergyMixin>();
+            
+        }
+    }
+}
