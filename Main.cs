4fac743e792de9fa96c8ddb46202beb7bb25047d
bakemonawa <86772183+bakemonawa@users.noreply.github.com>
2021-07-22 12:24:06 +0800

    Add files via upload

--- Main.cs
+++ Main.cs
@@ -2,21 +2,22 @@
 using HarmonyLib;
 using QModManager.API.ModLoading;
 using Logger = QModManager.Utility.Logger;
+using System;
 
 namespace LaserCutterMk2
 {
     [QModCore]
         public static class Main
         {
-
-        
-       
+                   
         
         [QModPatch]
             public static void Load()
             {
 
-            LaserCutterMk2.Patch();
+            var lasercuttermk2 = new LaserCutterMk2Prefab("LaserCutterMk2", "Laser Cutter Mk 2", "Removes built-in safety features, allowing the Laser Cutter to be used on organic targets");
+            lasercuttermk2.Patch();
+
 
                 Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.falselight.subnautica.lasercutterimprovements.mod");
                 Logger.Log(Logger.Level.Info, "Patched successfully.");
